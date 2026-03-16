b161053732991e094478cb5d58f5089a8a494e86
gigumay <gigumay@gmail.com>
2026-03-16 21:04:01 +0100

    not sure

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -280,11 +280,12 @@ class v8DetectionLoss:
         for v in self.global_obj_protos.values():
             assert len(v.shape) == 2 and v.shape[0] == self.hyp.n_obj_protos
 
-        # load global prototypes
-        self.global_bg_protos = {"backbone": torch.load(self.hyp.global_bg_protos["backbone"]).to(device),
-                                 "head": torch.load(self.hyp.global_bg_protos["head"]).to(device)}
-        for v in self.global_bg_protos.values():
-            assert len(v.shape) == 2 and v.shape[0] == self.hyp.n_bg_protos
+        if self.hyp.use_background:
+            # load global prototypes
+            self.global_bg_protos = {"backbone": torch.load(self.hyp.global_bg_protos["backbone"]).to(device),
+                                    "head": torch.load(self.hyp.global_bg_protos["head"]).to(device)}
+            for v in self.global_bg_protos.values():
+                assert len(v.shape) == 2 and v.shape[0] == self.hyp.n_bg_protos
 
         self.stride = m.stride  # model strides
         self.nc = m.nc  # number of classes
